9c69fc9652162a16ec26637cb215305134c0a9c8
Carolina Lezcano <lezcanocarolina99@gmail.com>
2022-08-03 23:52:42 -0300

    agregado de objetos arrays evento y funcion

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-function mostrar()
-{
-    let usdArs;
-    let usdEur;
-    let usdBrl;
-    let arsUsd;
-    let arsEur;
-    let arsBrl;
-    let eurUsd;
-    let eurBrl;
-    let eurArs;
-    let brlUsd;
-    let brlEur;
-    let brlArs;
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", mostrar);
+
+function mostrar() {
     let montoAConvertir;
     let convertirDe;
     let convertirA;
     let montoConvertido;
     let tasaDeCambio;
     let mensaje;
+    let montoTipoCambio;
+    let tipoDeCambio;
+    let respuesta;
+    let transacciones = [];
+
+
+    montoTipoCambio = {
+        usdArs: 130.89,
+        usdEur: 0.98,
+        usdBrl: 5.24,
+        arsUsd: 0.0076,
+        arsEur: 0.0075,
+        arsBrl: 0.04,
+        eurUsd: 1.02,
+        eurBrl: 5.35,
+        eurArs: 133.51,
+        brlUsd: 0.19,
+        brlEur: 0.19,
+        brlArs: 24.96,
+    };
 
     usdArs = 130.89;
     usdEur = 0.98;
     usdBrl = 5.24;
     arsUsd = 0.0076;
     arsEur = 0.0075;
-    arsBrl = 0.040;
+    arsBrl = 0.04;
     eurUsd = 1.02;
     eurBrl = 5.35;
     eurArs = 133.51;
@@ -32,84 +44,120 @@ function mostrar()
     brlEur = 0.19;
     brlArs = 24.96;
 
-    montoAConvertir = prompt("Ingrese el monto a convertir: ");
-    montoAConvertir = parseInt(montoAConvertir);
+    respuesta = "si";
 
-    convertirDe = prompt("Ingrese  a convertir: (dolar/peso/real/euro): ");
-    while(convertirDe != "euro" && convertirDe != "peso" && convertirDe != "dolar" && convertirDe != "real")
-    {
+    while (respuesta == "si") {
+
+        montoAConvertir = prompt("Ingrese el monto a convertir: ");
+        montoAConvertir = parseInt(montoAConvertir);
+
         convertirDe = prompt("Ingrese  a convertir: (dolar/peso/real/euro): ");
+        while (
+            convertirDe != "euro" &&
+            convertirDe != "peso" &&
+            convertirDe != "dolar" &&
+            convertirDe != "real"
+        ) {
+            convertirDe = prompt("Ingrese  a convertir: (dolar/peso/real/euro): ");
+        }
+
+        convertirA = prompt(
+            "Ingrese  que desea a cambio: (dolar/peso/real/euro): "
+        );
+        while (
+            convertirA != "euro" &&
+            convertirA != "peso" &&
+            convertirA != "dolar" &&
+            convertirA != "real"
+        ) {
+            convertirA = prompt(
+                "Ingrese  que desea a cambio: (dolar/peso/real/euro): "
+            );
+        }
+
+        switch (convertirDe) {
+            case "euro":
+                switch (convertirA) {
+                    case "dolar":
+                        tasaDeCambio = montoTipoCambio.eurUsd;
+                        break;
+                    case "peso":
+                        tasaDeCambio = montoTipoCambio.eurArs;
+                        break;
+                    case "real":
+                        tasaDeCambio = montoTipoCambio.eurBrl;
+                        break;
+                }
+                break;
+            case "dolar":
+                switch (convertirA) {
+                    case "euro":
+                        tasaDeCambio = montoTipoCambio.usdEur;
+                        break;
+                    case "peso":
+                        tasaDeCambio = montoTipoCambio.usdArs;
+                        break;
+                    case "real":
+                        tasaDeCambio = montoTipoCambio.usdBrl;
+                        break;
+                }
+                break;
+            case "peso":
+                switch (convertirA) {
+                    case "euro":
+                        tasaDeCambio = montoTipoCambio.arsEur;
+                        break;
+                    case "dolar":
+                        tasaDeCambio = montoTipoCambio.arsUsd;
+                        break;
+                    case "real":
+                        tasaDeCambio = montoTipoCambio.arsBrl;
+                        break;
+                }
+                break;
+            case "real":
+                switch (convertirA) {
+                    case "euro":
+                        tasaDeCambio = montoTipoCambio.brlEur;
+                        break;
+                    case "dolar":
+                        tasaDeCambio = montoTipoCambio.brlUsd;
+                        break;
+                    case "peso":
+                        tasaDeCambio = montoTipoCambio.brlArs;
+                        break;
+                }
+                break;
+        }
+
+        montoConvertido = convertirMonto(montoAConvertir, tasaDeCambio);
+        mensaje =
+            "El monto a convertir de " +
+            montoAConvertir.toFixed(2) +
+            " " +
+            convertirDe +
+            ", son " +
+            montoConvertido.toFixed(2) +
+            " " +
+            convertirA;
+
+        alert(mensaje);
+
+        respuesta = prompt("desea seguir realizando transacciones?")
+        while (respuesta != "si" && respuesta != "no") {
+            respuesta = prompt("desea seguir realizando transacciones? si/no")
+        }
+
+        transacciones.push(mensaje);
     }
 
-    convertirA = prompt("Ingrese  que desea a cambio: (dolar/peso/real/euro): ");
-    while(convertirA != "euro" && convertirA != "peso" && convertirA != "dolar" && convertirA != "real")
-    {
-        convertirA = prompt("Ingrese la moneda que desea a cambio: (dolar/peso/real/euro): ");
-    }
+    resumenTransacciones = "Resumen Transacciones: " + "<br>" + transacciones.forEach(transaccion => transaccion + "<br>")
 
-    switch(convertirDe)
-    {
-        case "euro":
-            switch(convertirA)
-            {
-                case "dolar":
-                    tasaDeCambio = eurUsd;
-                    break;
-                case "peso":
-                    tasaDeCambio = eurArs;
-                    break;
-                case "real":
-                    tasaDeCambio = eurBrl;
-                    break;
-            }
-            break;
-        case "dolar":
-            switch(convertirA)
-            {
-                case "euro":
-                    tasaDeCambio = usdEur;
-                    break;
-                case "peso":
-                    tasaDeCambio = usdArs;
-                    break;
-                case "real":
-                    tasaDeCambio = usdBrl;
-                    break;
-            }
-            break;
-        case "peso":
-            switch(convertirA)
-            {
-                case "euro":
-                    tasaDeCambio = arsEur;
-                    break;
-                case "dolar":
-                    tasaDeCambio = arsUsd;
-                    break;
-                case "real":
-                    tasaDeCambio = arsBrl;
-                    break;
-            }
-            break;
-        case "real":
-            switch(convertirA)
-            {
-                case "euro":
-                    tasaDeCambio = brlEur;
-                    break;
-                case "dolar":
-                    tasaDeCambio = brlUsd;
-                    break;
-                case "peso":
-                    tasaDeCambio = brlArs;
-                    break;
-            }
-            break;
-    }
+    document.write(transacciones);
+}
 
-    montoConvertido = montoAConvertir * tasaDeCambio;
-    mensaje = "El monto a convertir de " + montoAConvertir + " " + convertirDe + ", son " + montoConvertido + " " + convertirA;
+function convertirMonto(montoAConvertir, tasaDeCambio) {
+    let montoConvertido = montoAConvertir * tasaDeCambio;
 
-    alert(mensaje);
-    
+    return montoConvertido;
 }
