d048b40875726182ae882980a1d6941d3d1e0f79
Carolina Lezcano <lezcanocarolina99@gmail.com>
2022-08-19 14:26:00 -0300

    reentrega- segunda preentrega proyecto final

--- script.js
+++ script.js
@@ -1,134 +1,194 @@
+const APIKEY = '78ebf190f1e57d5cf73fea9e';
 
-let btn = document.getElementById("btn");
+const listaDesplegable = document.querySelectorAll('form select'),
+    primeraMoneda = document.querySelector('#primeraMoneda'),
+    segundaMoneda = document.querySelector('#segundaMoneda'),
+    btnConvertir = document.querySelector('#btnConversion'),
+    monto = document.querySelector('#monto'),
+    conversionTxt = document.querySelector('#conversionTxt'),
+    btnInvertirMoneda = document.querySelector('#icono'),
+    btn = document.getElementById("btn"),
+    clases = document.querySelectorAll(".arrayDeClases"),
+    logemail = document.getElementById("logemail"),
+    logpass = document.getElementById("logpass"),
+    login = document.getElementById("login");
 
-btn.addEventListener("click", mostrar)
-
-function mostrar() {
-    let montoConvertido
-    let tasaDeCambio;
-    let mensaje;
-    let montoTipoCambio;
-    let tipoDeCambio;
-    let transacciones = [];
-
-
-    montoTipoCambio = {
-        usdArs: 130.89,
-        usdEur: 0.98,
-        usdBrl: 5.24,
-        arsUsd: 0.0076,
-        arsEur: 0.0075,
-        arsBrl: 0.04,
-        eurUsd: 1.02,
-        eurBrl: 5.35,
-        eurArs: 133.51,
-        brlUsd: 0.19,
-        brlEur: 0.19,
-        brlArs: 24.96,
-    };
-
-    let montoAConvertir = document.querySelector("#montoAConvertir").value;
-    montoAConvertir = parseInt(montoAConvertir);
-    let convertirDe = document.querySelector("#convertirDe").value;
-    let convertirA = document.querySelector("#convertirA").value;
-    // let montoConvertido = document.querySelector("#montoConvertido").value;
-
-    usdArs = 130.89;
-    usdEur = 0.98;
-    usdBrl = 5.24;
-    arsUsd = 0.0076;
-    arsEur = 0.0075;
-    arsBrl = 0.04;
-    eurUsd = 1.02;
-    eurBrl = 5.35;
-    eurArs = 133.51;
-    brlUsd = 0.19;
-    brlEur = 0.19;
-    brlArs = 24.96;
+const usuarios = [{
+    nombre: 'Belen',
+    mail: 'belen@mail.com',
+    pass: 'belen123'
+},
+{
+    nombre: 'Franco',
+    mail: 'franco@mail.com',
+    pass: 'franco123'
+}]
 
 
-    switch (convertirDe) {
-        case "euro":
-            switch (convertirA) {
-                case "dolar":
-                    tasaDeCambio = montoTipoCambio.eurUsd;
-                    break;
-                case "peso":
-                    tasaDeCambio = montoTipoCambio.eurArs;
-                    break;
-                case "real":
-                    tasaDeCambio = montoTipoCambio.eurBrl;
-                    break;
-            }
-            break;
-        case "dolar":
-            switch (convertirA) {
-                case "euro":
-                    tasaDeCambio = montoTipoCambio.usdEur;
-                    break;
-                case "peso":
-                    tasaDeCambio = montoTipoCambio.usdArs;
-                    break;
-                case "real":
-                    tasaDeCambio = montoTipoCambio.usdBrl;
-                    break;
-            }
-            break;
-        case "peso":
-            switch (convertirA) {
-                case "euro":
-                    tasaDeCambio = montoTipoCambio.arsEur;
-                    break;
-                case "dolar":
-                    tasaDeCambio = montoTipoCambio.arsUsd;
-                    break;
-                case "real":
-                    tasaDeCambio = montoTipoCambio.arsBrl;
-                    break;
-            }
-            break;
-        case "real":
-            switch (convertirA) {
-                case "euro":
-                    tasaDeCambio = montoTipoCambio.brlEur;
-                    break;
-                case "dolar":
-                    tasaDeCambio = montoTipoCambio.brlUsd;
-                    break;
-                case "peso":
-                    tasaDeCambio = montoTipoCambio.brlArs;
-                    break;
-            }
-            break;
+
+function guardarDatos(usuarioDB, storage) {
+
+    const usuario = {
+        'name': usuarioDB.nombre,
+        'user': usuarioDB.mail,
+        'pass': usuarioDB.pass
     }
 
-    montoConvertido = convertirMonto(montoAConvertir, tasaDeCambio);
-    mensaje =
-        "El monto a convertir de " +
-        montoAConvertir.toFixed(2) +
-        " " +
-        convertirDe +
-        ", son " +
-        montoConvertido.toFixed(2) +
-        " " +
-        convertirA
+    storage.setItem('usuario', JSON.stringify(usuario));
+}
 
-    alert(mensaje);
+function validarUsuario(usersDB, user, pass) {
+    let encontrado = usersDB.find((userDB) => userDB.mail == user);
 
-    resumenTransacciones = "Resumen Transacciones: " + "<br>" + transacciones.forEach(transaccion => resumenTransacciones + "<br>");
+    if (typeof encontrado === 'undefined') {
+        return false;
+    } else {
+        if (encontrado.pass != pass) {
+            return false;
+        } else {
+            return encontrado;
+        }
+    }
+} 
 
-    document.write(transacciones);
+function recuperarUsuario(storage) {
+    return JSON.parse(storage.getItem('usuario'));
+}
 
-    sessionStorage.setItem("conversion1", montoConvertido);
+function intercambiarClases(array, clase) {
+    array.forEach(element => {
+        element.classList.toggle(clase);
+    })
 }
 
 
-function convertirMonto(montoAConvertir, tasaDeCambio) {
-    let montoConvertido = montoAConvertir * tasaDeCambio;
+login.addEventListener("click", (e) => {
+    Swal.fire("Bienvenido")
+    e.preventDefault()
+    if (!logemail.value || !logpass.value) {
+        alert('Complete todos los campos');
 
-    return montoConvertido;
+    } else {
+        let data = validarUsuario(usuarios, logemail.value, logpass.value);
 
-    conversion1 = sessionStorage.getItem("conversion1", montoConvertido);
-    alert(conversion1);
+        if (!data) {
+            alert(`Usuario y/o contraseña invalido`);
+        } else {
+
+            guardarDatos(data, localStorage);
+
+            intercambiarClases(clases, "noMostrar");
+        }
+    }
+
+})
+
+
+
+const crearSelectsMonedas = async () => {
+    const respuesta = await fetch('api.json');
+    const dataJson = await respuesta.json();
+
+    listaDesplegable.forEach((element, index) => {
+        for (const item of dataJson) {
+            let predet = (index == 0) ? ((item.moneda == 'ARS') ? 'selected' : '') : ((item.moneda == 'USD') ? 'selected' : '');
+            let optionHTML = `<option value="${item.moneda}" ${predet}>${item.moneda}</option>`;
+            element.insertAdjacentHTML('beforeend', optionHTML);
+        }
+
+
+        element.addEventListener('change', e => {
+            mostrarBandera(e.target);
+        })
+    })
+
 }
+
+crearSelectsMonedas();
+
+const mostrarBandera = async (element) => {
+    const respuesta = await fetch('api.json');
+    const dataJson = await respuesta.json();
+
+    for (const item of dataJson) {
+
+
+        if (item.moneda == element.value) {
+            let imagen = element.parentElement.querySelector('img');
+            imagen.src = `https://www.countryflagsapi.com/png/${item.pais}`;
+        }
+    }
+
+
+}
+
+function obtenerTipoCambio() {
+    let montoVal = monto.value;
+    if (montoVal == '' || montoVal == '0') {
+        monto.value = '1';
+        montoVal = 1;
+    }
+
+
+    conversionTxt.innerText = 'Esperando resultado...';
+
+    const URL = `https://v6.exchangerate-api.com/v6/${APIKEY}/latest/${primeraMoneda.value}`
+
+    fetch(URL)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result.conversion_rates);
+            let tasaConversion = result.conversion_rates[primeraMoneda.value];
+            let resultado = (montoVal * tasaConversion).toFixed(2);
+            conversionTxt.innerText = `${montoVal} ${primeraMoneda.value} = ${resultado} ${segundaMoneda.value}`;
+        }).catch(() => {
+            conversionTxt.innerText = 'Error, intente  de nuevo';
+        });
+
+}
+
+
+window.onload = () => {
+    obtenerTipoCambio();
+}
+
+btnConvertir.addEventListener('click', (e) => {
+    e.preventDefault();
+    obtenerTipoCambio();
+})
+
+btnInvertirMoneda.addEventListener('click', () => {
+    let temp = primeraMoneda.value;
+    primeraMoneda.value = segundaMoneda.value;
+    segundaMoneda.value = temp;
+    mostrarBandera(primeraMoneda);
+    mostrarBandera(segundaMoneda);
+    obtenerTipoCambio();
+})
+
+
+async function cambiar() {
+    let montoVal = monto.value;
+    if (montoVal == '' || montoVal == '0') {
+        monto.value = '1';
+        montoVal = 1;
+    }
+    conversionTxt.innerText = 'Esperando resultado...';
+    const URL = `https://v6.exchangerate-api.com/v6/${APIKEY}/latest/${primeraMoneda.value}`;
+
+    try {
+        const respuesta = await fetch(URL);
+        const data = await respuesta.json();
+        console.log(data.conversion_rates);
+        let tasaConversion = data.conversion_rates[segundaMoneda.value];
+        let resultado = (montoVal * tasaConversion).toFixed(2);
+        conversionTxt.innerText = `${montoVal} ${primeraMoneda.value} = ${resultado} ${segundaMoneda.value}`;
+
+    } catch (e) {
+        conversionTxt.innerText = 'Error, intente de nuevo';
+    }
+
+}
+
+
 
